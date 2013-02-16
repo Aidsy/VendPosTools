1e734b6f0c2a0784b6a35b69f0a4df2dc6ca1aed
Lachlan Davies <ld.is.me@gmail.com>
2013-02-16 15:45:22 +1100

    Update CSVBarcode.py

--- CSVBarcode.py
+++ CSVBarcode.py
@@ -1,29 +1,24 @@
-from reportlab.graphics.barcode import code39
-from reportlab.lib.pagesizes import A4
-from reportlab.lib.units import mm
-from reportlab.pdfgen import canvas
 import csv
 
-c = canvas.Canvas("barcode_example.pdf", pagesize=A4)
+#Get items from "codes.csv" and convert to a Python dictonary
+#File should have first column as item name, second as barcode number
+reader = csv.reader(open("codes.csv", "rb"))
+code_dict = {k: v for k, v in reader}
 
-code_list = 
+from reportlab.graphics.barcode import code39
+from reportlab.lib.units import mm
+from reportlab.pdfgen import canvas
 
-x = 1 * mm
-y = 285 * mm
-x1 = 6.4 * mm
 
-for code in code_list:
-    barcode = code39.Extended39(code)
-    barcode.drawOn(c, x, y)
-    x1 = x + 6.4 * mm
-    y = y - 5 * mm
-    c.drawString(x1, y, code)
-    x = x
-    y = y - 10 * mm
+x= 1 * mm
+y = 20 * mm
 
-    if int(y) == 0:
-        x = x + 50 * mm
-        y = 285 * mm
 
-c.showPage()
-c.save()
+for code in code_dict:
+    c = canvas.Canvas(code + ".pdf",pagesize=(80*mm,y))
+    barcode = code39.Extended39(code,barWidth=x,barHeight=y)
+    barcode.drawOn(c,-9,0)
+    c.showPage()
+    c.save()
+
+
